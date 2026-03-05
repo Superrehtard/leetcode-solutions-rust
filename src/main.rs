56153ab19bcdca76094be259982fd5ab29e21cb9
Pruthvi Parne <superrehtard@gmail.com>
2026-03-05 13:05:25 -0600

    add p209 solution

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod search_insert;
 mod plus_one;
 mod p58;
+mod p209;
 
-use crate::search_insert::search_insert_35::search_insert_35;
 use crate::plus_one::plus_one;
 use p58::length_of_last_word;
+use p209::min_sub_array_len;
 
 fn main() {
     let nums = vec![9,8,7,6,5,4,3,2,1,0];
@@ -16,4 +17,8 @@ fn main() {
 
     let mut iter = "".split_ascii_whitespace();
     println!("{:?}", iter.next());
+
+    let num_array = vec![1,2,3,4,5];
+
+    println!("{}", min_sub_array_len(11, num_array));
 }
---
+++ src/p209/mod.rs
@@ -0,0 +1,3 @@
+pub mod min_sub_array_len;
+
+pub use min_sub_array_len::min_sub_array_len;
